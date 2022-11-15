0b19186507eb935b53e1135171fa77aa0e21df2c
Muhammad Umair <181370124@gift.edu.pk>
2022-11-15 23:46:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import * as React from "react";
-import { Add, Settings } from "@mui/icons-material";
-import { Typography, Button } from "@mui/material";
 import theme from "./theme";
 
 import "@fontsource/roboto/300.css";
@@ -10,46 +8,33 @@ import "@fontsource/roboto/700.css";
 import styled from "@emotion/styled";
 
 function App() {
-  const BlueButton = styled(Button)(({ theme }) => ({
-    backgroundColor: "#000",
-    color: "#888",
-    margin: 5,
-    "&:hover": { backgroundColor: "lightblue" },
-    "&:disabled": { backgroundColor: "gray", color: "white" },
-  }));
-
-  return (
-    <div className="App">
-      <div className="main-div">
-        <Button variant="text">Text</Button>
-        <Button startIcon={<Settings />} variant="contained" color="primary">
-          Settings
-        </Button>
-        <Button startIcon={<Add />} variant="contained" color="success">
-          Add new post
-        </Button>
-        <Button variant="outlined" size="small">
-          Outlined
-        </Button>
-        <Typography variant="h2" component="p">
-          it uses h2 style but its a paragraph tag
-        </Typography>
-        <Button
-          variant="contained"
-          sx={{
-            backgroundColor: "skyblue",
-            color: "#888",
-            margin: 5,
-            "&:hover": { backgroundColor: "lightblue" },
-            "&:disabled": { backgroundColor: "gray", color: "white" },
-          }}
-        >
-          My Unique Button
-        </Button>
-        <BlueButton variant="contained">My Button</BlueButton>
-        <BlueButton variant="contained">My Button</BlueButton>
-      </div>
-    </div>
-  );
+  return <div className="App">Start App</div>;
 }
 export default App;
+
+// const BlueButton = styled(Button)(({ theme }) => ({
+//   backgroundColor: "#000",
+//   color: "#888",
+//   margin: 5,
+//   "&:hover": { backgroundColor: "lightblue" },
+//   "&:disabled": { backgroundColor: "gray", color: "white" },
+// }));
+
+//   <div className="main-div">
+// <Button variant="text">Text</Button>
+// <Button startIcon={<Settings />} variant="contained" color="secondary">
+//   Settings
+// </Button>
+// <Button startIcon={<Add />} variant="contained" color="success">
+//   Add new post
+// </Button>
+// <Button variant="outlined" size="small">
+//   Outlined
+// </Button>
+// <Typography variant="h2" component="p">
+//   it uses h2 style but it's a paragraph tag
+// </Typography>
+// <BlueButton variant="contained">My Button</BlueButton>
+//   <BlueButton variant="contained">My Another Button</BlueButton>
+// </div> */
+// }
